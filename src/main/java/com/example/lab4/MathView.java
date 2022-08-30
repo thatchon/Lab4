eaa00cff9e201d6faae1f256baa65cf0957bd11f
thatchon <63070067@it.kmitl.ac.th>
2022-08-30 10:39:04 +0700

    lab4

--- src/main/java/com/example/lab4/MathView.java
+++ src/main/java/com/example/lab4/MathView.java
@@ -1,4 +1,13 @@
 package com.example.lab4;
 
-public class MathView {
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.router.Route;
+
+
+@Route(value = "index")
+public class MathView extends VerticalLayout {
+    private TextField tn1, tn2, ans;
+    private Button test;
 }
